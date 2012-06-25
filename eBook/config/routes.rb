9022d84d9c8be23e49a196998dfa76178634bc7d
Gregory M. Silva <reptil9999@gmail.com>
2012-06-24 23:39:22 -0300

    Mudança de index e criação do controller para home

--- eBook/config/routes.rb
+++ eBook/config/routes.rb
@@ -1,4 +1,6 @@
 EBook::Application.routes.draw do
+  get "home/index"
+
   resources :ebooks
 
   resources :compras
@@ -54,7 +56,7 @@ EBook::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'Home#index'
 
   # See how all your routes lay out with "rake routes"
 
