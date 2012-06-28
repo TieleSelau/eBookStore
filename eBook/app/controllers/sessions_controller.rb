ed5e7e80ead63b216eaf174e7bfb4ba52d5ff323
Gregory Miola da Silva <reptil9999@gmail.com>
2012-06-28 14:37:29 -0300

    Sessao de usu�rio ok

--- eBook/app/controllers/sessions_controller.rb
+++ eBook/app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_username(params[:username])
+    user = User.find_by_nome(params[:username])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to root_url, :notice => "Logado!"
--- eBook/app/models/user.rb
+++ eBook/app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ActiveRecord::Base
   attr_accessible :admin, :data_nascimento, :email, :genero, :nome, :senha
+  
+  validate :email, :senha, :nome, :admin, :presence => true
+  validate :email, :nome, :uniqueness => true
+  
+  def authenticate senha
+    #user = find_by_username(username)
+    if self && self.senha == senha
+      self
+    else
+      nil
+    end
+  end
 end
